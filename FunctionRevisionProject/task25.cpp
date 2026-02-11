1d259c1b7fd73007d73c1b988e56e7d7117797db
polin-sv <polinasvetlay@icloud.com>
2026-02-11 18:43:37 +0300

    update

--- FunctionRevisionProject/task25.cpp
+++ FunctionRevisionProject/task25.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int  factorial(int n) {
     int result = 1;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         result *= i;
     }
     return result;
